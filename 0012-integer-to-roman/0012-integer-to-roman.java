774cd0fc6c5c6af6b731e8cd4965ecaeb768e41a
DARSHAN_M_RD <darshanmrd17@gmail.com>
2024-04-16 11:15:54 +0530

    Time: 11 ms (22.23%), Space: 44.7 MB (29.90%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String intToRoman(int num) {
+        String[] I={"","I","II","III","IV","V","VI","VII","VIII","IX"};
+        String[] X={"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+        String[] C={"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+        String[] M={"","M","MM","MMM"}; 
+        
+     
+            String str=M[(num/1000)]+
+                C[((num%1000)/100)]+
+                X[((num%100)/10)]+
+                I[(num%10)]
+            ;
+        return str;   
+    }
+}
