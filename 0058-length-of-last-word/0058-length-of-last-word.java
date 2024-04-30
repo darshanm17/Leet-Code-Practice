ab43d5efa47b5ddf77a8cf078483510ab59e87b3
DARSHAN_M_RD <darshanmrd17@gmail.com>
2024-04-30 14:32:58 +0530

    Time: 1 ms (30.97%), Space: 41.2 MB (95.72%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s=s.trim();
+        
+
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i) == ' '){
+                System.out.println(s);
+                return (s.length()-i-1);
+            }
+                
+        }
+
+        return s.length();
+        
+    }
+    }
