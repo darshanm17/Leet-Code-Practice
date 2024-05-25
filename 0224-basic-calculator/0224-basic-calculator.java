c58330553e38e89f65930452faef5179ba3d7ad0
DARSHAN_M_RD <darshanmrd17@gmail.com>
2024-05-25 17:34:58 +0530

    Time: 8 ms (76.61%), Space: 43.4 MB (68.71%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int calculate(String s) {
+              final Stack<Integer> stack = new Stack<>();
+
+        int sign = 1, number = 0, result = 0;
+
+        for(int i = 0; i < s.length(); ++i) {
+            final char c = s.charAt(i);
+
+            if(Character.isDigit(c)) {
+                number = 10 * number + c - '0';
+            } else if(c == '+') {
+                result += sign * number;
+                number = 0;
+                sign = 1;
+            } else if(c == '-') {
+                result += sign * number;
+                number = 0;
+                sign = -1;
+            } else if(c == '(') {
+                stack.push(result);
+                stack.push(sign);
+                sign = 1;
+                result = 0;
+            } else if(c == ')') {
+                result += sign * number;
+                number = 0;
+                result *= stack.pop();
+                result += stack.pop();
+            }
+        }
+
+        if(number != 0)
+            result += sign * number;
+
+        return result;
+    }
+}
