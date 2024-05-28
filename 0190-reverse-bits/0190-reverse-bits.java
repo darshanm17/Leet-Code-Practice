81208e6fe3c3bb4ad19f0b463ae783cc94d00655
DARSHAN_M_RD <darshanmrd17@gmail.com>
2024-05-28 12:28:42 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (38.14%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int reverseBits(int n) {
+        int ans = 0;
+
+        for (int i = 0; i < 32; i++) {
+            ans |= (n & 1) << (31 - i);
+            n >>= 1;
+        }
+        
+        return ans;
+    }
+}
