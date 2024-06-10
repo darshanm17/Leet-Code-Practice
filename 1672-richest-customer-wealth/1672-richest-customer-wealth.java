a96c671f7975aa7ff1891d3bbae295e59fea579b
DARSHAN_M_RD <darshanmrd17@gmail.com>
2024-06-10 13:35:53 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (32.36%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int n=accounts.length;
+        int m=accounts[0].length,max=Integer.MIN_VALUE;
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=0;j<m;j++){
+                sum+=accounts[i][j];
+            }
+            max=Math.max(sum,max);
+        }
+        return max;
+    }
+}
